15ff7a55e047d4102b00ab0f0a1305d18f532c60
student <student@tuffix-vm.myguest.virtualbox.org>
2024-10-31 18:57:53 -0700

    Finished

--- fib.c
+++ fib.c
@@ -3,35 +3,31 @@
 #include <string.h>
 #include <limits.h>
 
-#define MAX 100
-
-
-unsigned long long int memo[MAX];
-unsigned long long int fib_r_core(unsigned long long int n)
+unsigned long long int fib_r_core(int fibIndex)
 {
-    if (n == 0 || n == 1) {return 0;}
-    if (n == 2) {return 1;}
-    if (fib_r_core(n - 1) > ULLONG_MAX - fib_r_core(n - 2))
+    if (fibIndex == 0 || fibIndex == 1) {return 0;}
+    if (fibIndex == 2) {return 1;}
+    if (fib_r_core(fibIndex - 1) > ULLONG_MAX - fib_r_core(fibIndex - 2))
         {
-            printf("Error: overflow\nMax value: %llu\n", fib_r_core(n - 1));
-            exit(EXIT_FAILURE);
+            printf("Error: overflow\nMax value: %llu\n", fib_r_core(fibIndex - 1));
+            return 1;
         }
-    return fib_r_core(n - 1) + fib_r_core(n - 2);
+    return fib_r_core(fibIndex - 1) + fib_r_core(fibIndex - 2);
 }
 
-unsigned long long int fib_i_core(unsigned long long int n)
+unsigned long long int fib_i_core(int fibIndex)
 {
     unsigned long long int first = 0, second = 1, sum = 0;
-    if (n == 0) {return 0;}
-    if (n == 1 || n == 2) {return 1;}
+    if (fibIndex == 0) {return 0;}
+    if (fibIndex == 1 || fibIndex == 2) {return 0;}
     else
     {
-        for (int i = 2; i < n; ++i)
+        for (int i = 2; i < fibIndex; ++i)
         {
             if (second > ULLONG_MAX - first)
             {
                 printf("Error: overflow\nMax value: %llu\n", second);
-                exit(EXIT_FAILURE);
+                return 1;
             }
             sum = first + second;
             first = second;
@@ -41,54 +37,55 @@ unsigned long long int fib_i_core(unsigned long long int n)
     return sum;
 }
 
-unsigned long long int fib_r(unsigned long long int n)
-{
-    if (n == 0 || n == 1) {return 0;}
-    if (n == 2) {return 1;}
-    if (memo[n] != 0) {return memo[n];}
-    else
-    {
-        if (fib_r(n - 1) > ULLONG_MAX - fib_r(n - 2))
-        {
-            printf("Error: overflow\nMax value: %llu\n", fib_r(n - 1));
-            exit(EXIT_FAILURE);
+typedef unsigned long long int (*fib_func)(int);
+
+unsigned long long int *memcache = NULL;
+
+unsigned long long int memoize(fib_func func, unsigned long long int fibIndex) {
+    if (memcache == NULL) {
+        memcache = (unsigned long long int*)malloc((fibIndex + 1) * sizeof(unsigned long long int));
+        for (int i = 0; i <= fibIndex; i++) {
+            memcache[i] = -1;
         }
-        memo[n] = fib_r(n - 1) + fib_r(n - 2);
-        return memo[n];
+    }
+
+    if (memcache[fibIndex] == -1) {
+        memcache[fibIndex] = func(fibIndex);
+    }
+
+    return memcache[fibIndex];
+}
+
+unsigned long long int fib_r(int fibIndex) {
+    if (fibIndex == 0 || fibIndex == 1) {return 0;}
+    if (fibIndex == 2) {return 1;}
+    else 
+    {
+        if (memoize(fib_r, fibIndex - 1) > ULLONG_MAX - memoize(fib_r, fibIndex - 2))
+        {
+            printf("Error: overflow\nMax value: %llu\n", fib_r(fibIndex - 1));
+            return 1;
+        }
+        return memoize(fib_r, fibIndex - 1) + memoize(fib_r, fibIndex - 2);
     }
 }
 
-unsigned long long int fib_i(unsigned long long int n)
-{
-    unsigned long long int first = 0, second = 1;
-    if (n == 0) {return 0;}
-    if (n == 1 || n == 2) {return 1;}
-    if (memo[n] != 0) {return memo[n];}
-    else
-    {
-    	for (int i = 2; i < n; ++i)
-    	{
-            if (second > ULLONG_MAX - first)
-            {
-                printf("Error: overflow\nMax value: %llu\n", second);
-                exit(EXIT_FAILURE);
-            }
-    		memo[n] = first + second;
-    		first = second;
-    		second = memo[n];
-    	}
-    }
-    return memo[n];
-}
 
-int main(int argc, char *argv[])
-{
-    unsigned long long int num = atoi(argv[1]);
-    if (!strcmp(argv[2], "i"))
-    {
-        printf("%llu\n", fib_i(num));
+int main(int argc, char *argv[]) {
+    if (argc < 3) {
+        printf("./fib <number> <r or i>\n");
+        return 1;
+    }
+
+    unsigned long long int number_to_fib = atoi(argv[1]);
+
+    if (!strcmp(argv[2], "i")) {
+        printf("%llu\n", memoize(fib_i_core, number_to_fib));
     } else if (!strcmp(argv[2], "r")) {
-        printf("%llu\n", fib_r(num));
+        printf("%llu\n", memoize(fib_r, number_to_fib));
     }
+
+    free(memcache);
+
     return 0;
 }
